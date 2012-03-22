29399bc2e23d65625217d30468633e35f4c66321
Afsin Ustundag <austundag@raygroupintl.com>
2012-03-22 07:18:18 -0400

    Added new tests for Do command

--- com.raygroupintl.vista/test/com/raygroupintl/vista/mtoken/test/LineTest1.java
+++ com.raygroupintl.vista/test/com/raygroupintl/vista/mtoken/test/LineTest1.java
@@ -4,16 +4,20 @@ import static org.junit.Assert.fail;
 
 import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 
 import com.raygroupintl.vista.fnds.IFileAction;
 import com.raygroupintl.vista.mtoken.Line;
 import com.raygroupintl.vista.repository.MFileVisitor;
+import com.raygroupintl.vista.tools.MRoutineAnalyzer;
 
 import junit.framework.Assert;
 
 public class LineTest1 {
+	//private final static Logger LOGGER = Logger.getLogger(MRoutineAnalyzer.class.getName());
+
 	@Test
 	public void testAll() {
 		IFileAction action = new IFileAction() {			
@@ -22,8 +26,11 @@ public class LineTest1 {
 				try {
 					Scanner scanner = new Scanner(path);
 					int index = 0;
+					//if (! path.getFileName().toString().equals("PSSFDBRT.m")) return;
+					//LOGGER.info(path.getFileName().toString());
 					while (scanner.hasNextLine()) {
 						String line = scanner.nextLine();
+						//LOGGER.info(">>> " + line);
 						Line tokens = Line.getInstance(line);
 						String readLine = tokens.getStringValue();
 						String msg = path.getFileName().toString() + " Line " +  String.valueOf(index);
