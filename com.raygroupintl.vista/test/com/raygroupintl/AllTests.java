f685d4dc2e429c9265550c3168c5cd449b4675ac
Afsin Ustundag <austundag@raygroupintl.com>
2012-05-13 19:49:43 -0400

    All test suite

--- com.raygroupintl.vista/test/com/raygroupintl/AllTests.java
+++ com.raygroupintl.vista/test/com/raygroupintl/AllTests.java
@@ -4,11 +4,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.raygroupintl.bnf.GrammarTest;
-import com.raygroupintl.bnf.annotation.DescriptionSpecTest;
-import com.raygroupintl.m.token.LocalTests;
-
 @RunWith(Suite.class)
-@SuiteClasses({GrammarTest.class, DescriptionSpecTest.class, LocalTests.class})
+@SuiteClasses({
+	com.raygroupintl.bnf.AllTests.class, 
+	com.raygroupintl.bnf.annotation.AllTests.class, 
+	com.raygroupintl.m.token.AllTests.class, 
+	com.raygroupintl.vista.repository.AllTests.class, 
+	com.raygroupintl.vista.struct.AllTests.class, 
+	com.raygroupintl.vista.tools.AllTests.class 
+	})
 public class AllTests {
 }
