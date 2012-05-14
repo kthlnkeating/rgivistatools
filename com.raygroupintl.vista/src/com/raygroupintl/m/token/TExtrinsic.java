b336d5341a3c9fa1a441d6b558d4986b1eca689b
Afsin Ustundag <austundag@raygroupintl.com>
2012-05-14 10:31:54 -0400

    COde cleaning

--- com.raygroupintl.vista/src/com/raygroupintl/m/token/TExtrinsic.java
+++ com.raygroupintl.vista/src/com/raygroupintl/m/token/TExtrinsic.java
@@ -1,20 +1,10 @@
 package com.raygroupintl.m.token;
 
+import com.raygroupintl.bnf.TArray;
 import com.raygroupintl.bnf.Token;
-import com.raygroupintl.bnf.TCopy;
 
-public class TExtrinsic extends TCopy {
-	public TExtrinsic(Token source) {
-		super(source);
+public class TExtrinsic extends TArray {
+	public TExtrinsic(Token[] tokens) {
+		super(tokens);
 	}
-	
-	@Override
-	public String getStringValue() {
-		return "$$" + super.getStringValue();
-	}
-
-	@Override
-	public int getStringSize() {
-		return 2 + super.getStringSize();
-	}	
 }
