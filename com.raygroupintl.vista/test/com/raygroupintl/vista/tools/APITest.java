eb90d35eda05cc889f56907f71cc64dd9c4ad0cd
Afsin Ustundag <austundag@raygroupintl.com>
2012-06-20 17:00:32 -0400

    test preparation

---
+++ com.raygroupintl.vista/test/com/raygroupintl/vista/tools/APITest.java
@@ -0,0 +1,14 @@
+package com.raygroupintl.vista.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class APITest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
