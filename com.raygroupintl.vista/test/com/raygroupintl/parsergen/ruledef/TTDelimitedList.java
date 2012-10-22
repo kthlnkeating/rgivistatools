49a611f8c9cb9895fa9686f1500f63b9d54aca8a
Afsin Ustundag <austundag@raygroupintl.com>
2012-10-21 22:18:24 -0400

    TokenFactory is generized

---
+++ com.raygroupintl.vista/test/com/raygroupintl/parsergen/ruledef/TTDelimitedList.java
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------
+// Copyright 2012 Ray Group International
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//---------------------------------------------------------------------------
+
+package com.raygroupintl.parsergen.ruledef;
+
+import com.raygroupintl.parser.DelimitedListOfTokens;
+import com.raygroupintl.parser.Token;
+import com.raygroupintl.parser.Tokens;
+
+public class TTDelimitedList extends DelimitedListOfTokens<Token> implements Token {
+	public TTDelimitedList(Token leadingToken, Tokens<Token> tailTokens) {
+		super(leadingToken, tailTokens);
+	}
+
+	@Override
+	public void beautify() {
+		for (Token token : this.toIterable()) {
+			token.beautify();
+		}
+	}
+}
+
