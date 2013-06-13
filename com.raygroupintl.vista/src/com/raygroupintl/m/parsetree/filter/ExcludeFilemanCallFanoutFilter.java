07ab12baa66c83e3db2e8d07b2b19352f5bad6ca
austundag <afsin.ustundag@us.pwc.com>
2013-06-13 09:24:43 -0400

    Change of license

--- com.raygroupintl.vista/src/com/raygroupintl/m/parsetree/filter/ExcludeFilemanCallFanoutFilter.java
+++ com.raygroupintl.vista/src/com/raygroupintl/m/parsetree/filter/ExcludeFilemanCallFanoutFilter.java
@@ -1,4 +1,4 @@
-// Copyright 2012 Ray Group International
+// Copyright 2013 PwC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
